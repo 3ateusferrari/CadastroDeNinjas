dad4873e4d0512345348389cf9d00eb3ec2310c0
3ateusferrari <mateusspier@gmail.com>
2025-07-24 17:17:28 -0300

    feat: terminar em casa(sei que não é o commit ideal hehe)

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,13 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MissoesController {
+
+    @GetMapping("/boasvindas1")
+    public String boasVindas(){
+        return "Seja bem vindo1";
+    }
 }
