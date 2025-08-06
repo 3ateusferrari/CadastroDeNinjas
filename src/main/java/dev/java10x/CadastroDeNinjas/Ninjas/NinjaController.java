74e51e7a9ff26ecc57c6e3fba2bdc2868fe8bb97
3ateusferrari <mateusspier@gmail.com>
2025-08-06 16:13:23 -0300

    feat: Implementa crud de ninjas

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,7 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class NinjaController {
@@ -9,6 +8,37 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Seja bem vindo";
+    }
+
+
+    //Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "ninja criado";
+    }
+
+    // Mostrar ninja por id (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Mostrar todos os ninjas";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("todosID")
+    public String mostrarTodosOsNinjasPorId(){
+        return "Mostrar Ninja por Id";
+    }
+
+    //Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId(){
+        return "Alterar Ninja por Id";
+    }
+
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId(){
+        return "Deletar Ninja por Id";
     }
 
 }
