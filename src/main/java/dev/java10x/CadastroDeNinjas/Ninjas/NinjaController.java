b09da31c72a10f0798ef5c6a1cd9e3f95db7ffd5
3ateusferrari <mateusspier@gmail.com>
2025-08-07 12:51:59 -0300

    feat: Altera a rota do controller para listar os ninjas e injeta dependência do repo

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -2,8 +2,17 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(("/ninjas"))
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
@@ -18,13 +27,13 @@ public class NinjaController {
     }
 
     // Mostrar ninja por id (READ)
-    @GetMapping("/todos")
-    public String mostrarTodosOsNinjas(){
-        return "Mostrar todos os ninjas";
+    @GetMapping("/listar")
+    public List<NinjaModel> listarNinjas(){
+        return ninjaService.listarNinjas();
     }
 
     // Mostrar todos os ninjas (READ)
-    @GetMapping("todosID")
+    @GetMapping("listarId")
     public String mostrarTodosOsNinjasPorId(){
         return "Mostrar Ninja por Id";
     }
